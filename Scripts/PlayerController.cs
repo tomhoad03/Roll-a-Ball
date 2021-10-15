d2bd9414cc8fc6bb38512f2dc990321db911704b
Tom Hoad <40165392+tomhoad03@users.noreply.github.com>
2021-10-16 00:06:41 +0100

    Roll-a-Ball complete

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -2,16 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+    private int count;
 
     void Start() {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winTextObject.SetActive(false);
+    }
+
+    void setCountText() {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 12) {
+            winTextObject.SetActive(true);
+        }
     }
 
     void OnMove(InputValue movementValue) {
@@ -26,7 +41,11 @@ public class PlayerController : MonoBehaviour {
         rb.AddForce(movement * speed);
     }
 
-    void OnTriggerEnter(Collider other) {
-        
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Pickup")) {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
     }
 }
